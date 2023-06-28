c4556813bb3c42c0eb37a3c02fc442e4617470f4
Dioonel <dionelmartinezc@gmail.com>
2023-06-28 13:41:39 -0300

    Basic interface and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,12 @@ import { DataService } from './services/data.service';
 export class AppComponent {
   imageSelected: File | null = null;
   imagePreview!: string | ArrayBuffer | null;
-  outputFormat: 'png' | 'jpg' | 'jpeg' | 'webp' | 'gif' = 'png';
+  outputFormat: 'png' | 'jpg' | 'jpeg' | 'webp' | 'gif' | '' = '';
+  loading = false;
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService) {
+    console.log(this.outputFormat);
+  }
 
   onFileSelected(event: any) {
     this.imageSelected = event.target.files[0];
@@ -22,6 +25,7 @@ export class AppComponent {
       if (!validTypes.includes(this.imageSelected.type)) {
         alert('Formato de archivo no válido. Por favor, selecciona una imagen en formato PNG, JPG, JPEG, JFIF, WEBP O GIF.');
         this.imageSelected = null;
+        this.imagePreview = null;
         return;
       }
 
@@ -29,6 +33,7 @@ export class AppComponent {
       if (this.imageSelected.size > maxSize) {
         alert('File size exceeds 16 MB');
         this.imageSelected = null;
+        this.imagePreview = null;
         return;
       }
     }
@@ -42,6 +47,7 @@ export class AppComponent {
 
   convert() {
     if(this.imageSelected){
+      this.loading = true;
       const formData = new FormData();
       formData.append('image', this.imageSelected);
       formData.append('outputFormat', this.outputFormat);
@@ -51,6 +57,7 @@ export class AppComponent {
           next: (response: Blob) => {
             const url= window.URL.createObjectURL(response);
             console.log(url);
+            this.loading = false;
             // download the file
             const a = document.createElement('a');
             a.href = url;
@@ -60,6 +67,7 @@ export class AppComponent {
             a.remove();
           },
           error: (error) => {
+            this.loading = false;
             console.log(error);
           }
         }
